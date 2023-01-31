a91a7aec127214e99846d04bc5cf10cfad3c6c43
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-01 08:46:41 +0900

    postsテーブルにcontentカラム追加

---
+++ db/schema.rb
@@ -0,0 +1,10 @@
+ActiveRecord::Schema.define(version: 2023_01_31_233817) do
+  enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
