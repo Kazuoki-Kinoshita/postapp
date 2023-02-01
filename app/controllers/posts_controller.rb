0881e457e07250f028531f3c1a2b93747d23776b
kinoshita kazuoki <kino05040826@gmail.com>
2023-02-01 12:55:56 +0900

    createアクション作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,8 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  
+  def create
+    @post = Post.create(params.require(:post).permit(:content))
+    redirect_to new_post_path
+  end
 end
